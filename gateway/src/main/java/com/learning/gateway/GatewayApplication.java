e39d2b27be163a8ab3333696f98ffe7b64e54c0e
Kanchan Jakhar <jakhar.kanchan@gmail.com>
2021-02-03 17:03:15 +0530

    Add files via upload

---
+++ gateway/src/main/java/com/learning/gateway/GatewayApplication.java
@@ -0,0 +1,32 @@
+package com.learning.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import com.learning.gateway.filters.*;
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	@Bean
+	public PreFilter preFilter() {
+	    return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+	    return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+	    return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+	    return new RouteFilter();
+	}
+
+}
